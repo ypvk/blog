9a4b77aca8aa57c867ec644ba83ea0c49d5a8486
Yu Ping <yupingseu@gmail.com>
2012-11-19 14:39:21 +0800

    add model for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField()
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+class Articale(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    preview = models.TextField()
+    slug = models.CharField(max_length=10)
+    author = models.ForeignKey(User)
