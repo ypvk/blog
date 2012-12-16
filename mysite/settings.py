31650d81b9b0aaae6484f2dff387d052c696576a
Yu Ping <yupingseu@gmail.com>
2012-12-17 00:48:21 +0800

    add static config in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -70,6 +70,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_PATH + '../blog/static/css', #for css
+    PROJECT_PATH + '../blog/static/javascript', # for javascript
 )
 
 # List of finder classes that know how to find static files in
