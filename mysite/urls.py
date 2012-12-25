634a8feb18604ba07ae8e6605e13516354c617be
Yu Ping <yupingseu@gmail.com>
2012-12-25 14:16:17 +0800

    add url for registration

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ urlpatterns = patterns('',
     url(r'^users/', include('blog.urls.users')),
     url(r'^articles/', include('blog.urls.articles')),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html.haml'}, name='login'),
-    url(r'^logout/$', 'blog.views.users.user_logout', name="logout"),
+    url(r'^logout/$', 'blog.views.useraction.user_logout', name="logout"),
+    url(r'^registration/$', 'blog.views.useraction.registration', name='registration'),
 )
