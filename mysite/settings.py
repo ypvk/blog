26de5e581e34b51f1773505164e6000b2ed5418a
Yu Ping <yupingseu@gmail.com>
2012-12-25 23:08:17 +0800

    add login url

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,6 +55,9 @@ MEDIA_ROOT = ''
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
+# add login url
+LOGIN_URL = '/login/'
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
